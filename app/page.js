f8af60bb803c28ebc52db117597ba048071dfcb5
isaacdance2018-cmyk <isaacdance2018@gmail.com>
2026-05-09 23:13:27 +1000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,6 @@
 export default function Home() {
   return (
-    <main style={{
-      padding: "40px",
-      fontFamily: "Arial",
-      background: "#f5f1ea",
-      minHeight: "100vh"
-    }}>
+    <div>
       <h1>Saturn Art Dance Studio</h1>
 
       <h2>Mitcham 校区</h2>
@@ -15,6 +10,6 @@ export default function Home() {
       <p>5/445 Warrigal Rd, Moorabbin VIC 3189</p>
 
       <p>Email: saturndance2022@gmail.com</p>
-    </main>
+    </div>
   );
 }
