dbf306ae89cb3a95ec8cfc5a4b0825af10703624
isaacdance2018-cmyk <isaacdance2018@gmail.com>
2026-05-09 22:36:31 +1000

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,41 @@
+
+export default function Home() {
+  return (
+    <main style={{padding:"60px",fontFamily:"Arial"}}>
+      <h1>Saturn Art Dance Studio</h1>
+
+      <p>Professional Dance Education in Melbourne</p>
+
+      <hr />
+
+      <h2>Navigation</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Classes</li>
+        <li>Gallery</li>
+        <li>Team</li>
+        <li>Contact</li>
+      </ul>
+
+      <hr />
+
+      <h2>Contact</h2>
+
+      <p>
+        <strong>Mitcham校区：</strong>
+        21 Rooks Rd, Mitcham VIC 3132
+      </p>
+
+      <p>
+        <strong>Moorabbin校区：</strong>
+        5/445 Warrigal Rd, Moorabbin VIC 3189
+      </p>
+
+      <p>
+        <strong>Email：</strong>
+        saturndance2022@gmail.com
+      </p>
+    </main>
+  );
+}
