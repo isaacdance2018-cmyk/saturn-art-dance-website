d5cb18d9c10fa584fcdd2c046e1c96a3434b77bd
isaacdance2018-cmyk <isaacdance2018@gmail.com>
2026-05-09 22:42:43 +1000

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Saturn Art Dance Studio"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
